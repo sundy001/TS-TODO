5f5360953ebf589d94dafeddc69dc776a75368bb
Jason Li <kahli@ebay.com>
2019-07-17 00:42:37 +0200

    fix typescript

--- src/components/AddToDo/AddToDo.tsx
+++ src/components/AddToDo/AddToDo.tsx
@@ -1,14 +1,14 @@
 import React, { useRef } from "react";
 
 export const AddTodo: React.SFC<Props> = ({ addTodo }) => {
-  const inputRef = useRef<HTMLInputElement>();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault();
-          const input = inputRef.current;
+          const input = inputRef.current!;
           if (!input.value.trim()) {
             return;
           }
