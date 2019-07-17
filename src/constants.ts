a527eebb5f239e462186d4eae33d049975ad7eff
Jason Li <kahli@ebay.com>
2019-07-17 08:11:09 +0200

    type of state

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const VISIBILIT_FILTER = {
   SHOW_COMPLETED: "SHOW_COMPLETED",
   SHOW_ACTIVE: "SHOW_ACTIVE"
 };
+
+export type VisibilityFilter = "SHOW_ALL" | "SHOW_COMPLETED" | "SHOW_ACTIVE";
--- src/store/type.ts
+++ src/store/type.ts
@@ -1,1 +1,12 @@
-export {}; // this is a placeholder
+import { VisibilityFilter } from "../constants";
+
+export type State = Readonly<{
+  todos: readonly TodoItem[];
+  visibilityFilter: VisibilityFilter;
+}>;
+
+export type TodoItem = Readonly<{
+  id: number;
+  text: string;
+  completed: boolean;
+}>;
